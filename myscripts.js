7288a98cbaad4655ad1f3282ff95ce3b258c7ede
orionsView <davidhunterphillips03@gmail.com>
2025-02-21 20:17:29 -0800

    savePoint

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,38 @@
 const button = document.getElementById('but1');
 const text = document.getElementById('text1');
+const testingText = document.getElementById('text2');
 
 
 document.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', () => {
-        chrome.tabs.query({}, function(tabs) {
+        chrome.tabs.query({}, function (tabs) {
             console.log(tabs); // Logs an array of Tab objects
             // let urls = tabs.map(tab => tab.url)
-            
-            for(let i = 0; i < tabs.length; i++){
-                text.innerHTML += i +": " + tabs[i].url + "<br />";
+            let hashSet = new Set();
+
+            for (let i = 0; i < tabs.length; i++) {
+                let curURL = tabs[i].url;
+                if (hashSet.has(curURL)) {
+                    testingText.textContent = "FLIPPED"
+                    text.innerHTML += i + ": " + curURL + "<br />";
+                    text.style.color = 'red';
+                    openPopup();
+                } else {
+                    hashSet.add(curURL);
+                    text.innerHTML += i + ": " + curURL + "<br />";
+                }
+
             }
-        });        
+        });
     });
 });
+
+function openPopup() {
+    chrome.windows.create({
+        url: "hello.html",
+        type: "popup",
+        width: 200,
+        height: 150
+    });
+}
 
