92f211ad9ea40534dc966dbdfb75e0adb3f8bed9
David P <davidhunterphillips03@gmail.com>
2025-02-24 20:16:37 -0800

    extension now places tab in new page if you open it in new tab

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,60 @@
+let hashSet = new Set();
+chrome.tabs.query({}, function (tabs) {
+                // const text = document.getElementById("text1");
+                console.log(tabs); // Logs an array of Tab objects
+                // let urls = tabs.map(tab => tab.url)
+                
+    
+                for (let i = 0; i < tabs.length; i++) {
+                    let curURL = tabs[i].url;
+                    hashSet.add(curURL);
+                    // if (hashSet.has(curURL)) {
+                    //     // testingText.textContent = "FLIPPED"
+                    //     // text.innerHTML += i + ": " + curURL + "<br />";
+                    //     // text.style.color = 'red';
+                    //     // openPopup();
+                    // } else {
+                    //     hashSet.add(curURL);
+                    //     // text.innerHTML += i + ": " + curURL + "<br />";
+                    // }
+    
+                }
+            });
 
-let i = 0;
+let limit = 0;
 chrome.tabs.onCreated.addListener((tab) => {
-    i++;
-    if (i <= 1) {
-        console.log("New tab created:", tab.url);
-        openPopup();
-
+    let newURL
+    if (tab.pendingUrl) {
+        newURL = tab.pendingUrl;
+        console.log("Pending URL:", tab.pendingUrl);
+    } else if (tab.url) {
+        newURL = tab.url;
+        console.log("Final URL:", tab.url);
+    } else {
+        console.log("No URL available yet.");
     }
+    if (hashSet.has(newURL)) {
+        console.log("Already OPEN");
+
+        let tabID = tab.id;
+        console.log("ID: " + tabID);
+        chrome.tabs.group({ tabIds: tabID });
+    }
+    hashSet.add(newURL);
+
+    // let newURL = tab.pendingURL ? tab.pendingURL : tab.url; // newly created URL
+    // // let tabID = tab[0].ID;
+    // // if (hashSet.has(curURL)) {
+    //     console.log(newURL);
+    // }
 });
+
+// limit++;
+// if (limit <= 1) {
+//     console.log("New tab created:", tab.url);
+//     openPopup();
+
+// }
 
 // function openPopup() {
 //     console.log("Triggered");
@@ -18,3 +65,5 @@ chrome.tabs.onCreated.addListener((tab) => {
 //         height: 150
 //     });
 // }
+
+
